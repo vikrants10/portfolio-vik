5c0c366bff3a9aa5c6387029e1b21d4b41d25b5d
Vikrant Kumar Singh <vikrantkumarsingh333@gmail.com>
2024-10-17 15:02:27 +0530

    Nav.jsx

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -4,6 +4,7 @@ import {BiUser} from 'react-icons/bi'
 import {GiSkills} from 'react-icons/gi'
 import {RiServiceLine} from 'react-icons/ri'
 import { DiAtom } from "react-icons/di";
+import { PiOfficeChairLight } from "react-icons/pi";
 import {BiSolidMessageRoundedDetail} from 'react-icons/bi'
 import './nav.css'
 import { useState } from 'react'
@@ -16,6 +17,7 @@ const Nav = () => {
       <a href="#experience" onClick={()=>setActiveNav('#experience')} className = {activeNav==='#experience'? 'active':''}><GiSkills/></a>
       <a href="#services" onClick={()=>setActiveNav('#services')} className = {activeNav==='#services'? 'active':''}><RiServiceLine/></a>
       <a href="#portfolio" onClick={()=>setActiveNav('#portfolio')} className = {activeNav==='#portfolio'? 'active':''}><DiAtom/></a>
+      <a href="#testimonial" onClick={()=>setActiveNav('#testimonial')} className = {activeNav==='#testimonial'? 'active':''}><PiOfficeChairLight/></a>
       <a href="#contact" onClick={()=>setActiveNav('#contact')} className = {activeNav==='#contact'? 'active':''}><BiSolidMessageRoundedDetail/></a>
     </nav>
   )
