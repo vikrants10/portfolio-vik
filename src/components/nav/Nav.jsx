f3231a75a07a0c931ceb7d9c0343df5f609a1317
Vikrant Kumar Singh <vikrantkumarsingh333@gmail.com>
2024-10-17 14:40:13 +0530

    Nav.jsx

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -3,6 +3,7 @@ import {AiOutlineHome} from 'react-icons/ai'
 import {BiUser} from 'react-icons/bi'
 import {GiSkills} from 'react-icons/gi'
 import {RiServiceLine} from 'react-icons/ri'
+import { DiAtom } from "react-icons/di";
 import {BiSolidMessageRoundedDetail} from 'react-icons/bi'
 import './nav.css'
 import { useState } from 'react'
@@ -14,6 +15,7 @@ const Nav = () => {
       <a href="#about" onClick={()=>setActiveNav('#about')} className = {activeNav==='#about'? 'active':''}><BiUser/></a>
       <a href="#experience" onClick={()=>setActiveNav('#experience')} className = {activeNav==='#experience'? 'active':''}><GiSkills/></a>
       <a href="#services" onClick={()=>setActiveNav('#services')} className = {activeNav==='#services'? 'active':''}><RiServiceLine/></a>
+      <a href="#portfolio" onClick={()=>setActiveNav('#portfolio')} className = {activeNav==='#portfolio'? 'active':''}><DiAtom/></a>
       <a href="#contact" onClick={()=>setActiveNav('#contact')} className = {activeNav==='#contact'? 'active':''}><BiSolidMessageRoundedDetail/></a>
     </nav>
   )
